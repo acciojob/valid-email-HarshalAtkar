879b39d62769a8254bdf74cb92e52f0fc0a94ad8
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-01-03 21:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
+  //your JS code here.function validateEmail(email) {
+    const emailPattern = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
+    return emailPattern.test(str);
+}
+
 }
 
 // Do not change the code below.
